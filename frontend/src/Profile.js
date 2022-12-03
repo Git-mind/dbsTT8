efacf9543ec63c4c40ef2c7c7a33eea72b3899e8
ricsg <96154243+ricsg@users.noreply.github.com>
2022-12-03 16:10:27 +0800

    Edit doesnt really work
    doesnt go back to the edit

---
+++ frontend/src/Profile.js
@@ -0,0 +1,98 @@
+import { useEffect, useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+
+const Profile = () => {
+  const [profdata, profdatachange] = useState(null);
+  const navigate = useNavigate();
+
+  const LoadDetail = (id) => {
+    navigate("/profile/detail/" + id);
+  };
+  const LoadEdit = (id) => {
+    navigate("/profile/edit/" + id);
+  };
+  const Removefunction = (id) => {
+    if (window.confirm("Do you want to remove?")) {
+      fetch("http://localhost:8000/employee/" + id, {
+        method: "DELETE",
+      })
+        .then((res) => {
+          alert("Removed successfully.");
+          window.location.reload();
+        })
+        .catch((err) => {
+          console.log(err.message);
+        });
+    }
+  };
+
+  useEffect(() => {
+    fetch("https://mocki.io/v1/975c7fac-bb98-41ae-b9a0-374d3b72871f")
+      .then((res) => {
+        return res.json();
+      })
+      .then((resp) => {
+        profdatachange(resp);
+      })
+      .catch((err) => {
+        console.log(err.message);
+      });
+  }, []);
+  return (
+    <div className="container">
+      <div className="card">
+        <div className="card-title">
+          <h2>Profile Page</h2>
+        </div>
+        <div className="card-body">
+          <div className="divbtn"></div>
+          <table className="table table-bordered">
+            <thead className="bg-dark text-white">
+              <tr>
+                <td>Username</td>
+                <td>Firstname</td>
+                <td>Lastname</td>
+                <td>Email:</td>
+                <td>Address</td>
+                <td>OptIntoPhyStatements</td>
+              </tr>
+            </thead>
+            <tbody>
+              {profdata &&
+                profdata.map((item) => (
+                  <tr key={item.UserID}>
+                    <td>{item.Username}</td>
+                    <td>{item.Firstname}</td>
+                    <td>{item.Lastname}</td>
+                    <td>{item.Email}</td>
+                    <td>{item.Address}</td>
+                    <td>{item.OptIntoPhyStatements}</td>
+                    <td>
+                      <a
+                        onClick={() => {
+                          Removefunction(item.id);
+                        }}
+                        className="btn btn-danger"
+                      >
+                        Remove
+                      </a>
+                      <a
+                        onClick={() => {
+                          LoadEdit(item.id);
+                        }}
+                        className="btn btn-primary"
+                      >
+                        Edit
+                      </a>
+                    </td>
+                  </tr>
+                ))}
+            </tbody>
+          </table>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Profile;
---
+++ frontend/src/ProfileEdit.js
@@ -0,0 +1,162 @@
+import { useEffect, useState } from "react";
+import { Link, useNavigate, useParams } from "react-router-dom";
+
+const ProEdit = () => {
+  const { userid } = useParams();
+
+  //const [empdata, empdatachange] = useState({});
+  //put to edit localhost:5000/user/getUserDetails
+  useEffect(() => {
+    fetch("http://localhost:8000/employee/" + userid)
+      .then((res) => {
+        return res.json();
+      })
+      .then((resp) => {
+        idchange(resp.id);
+
+        resp.Firstname;
+        lastnamechange(resp.Lastname);
+        emailchange(resp.Email);
+        addresschange(resp.Address);
+        activechange(resp.isactive);
+      })
+      .catch((err) => {
+        console.log(err.message);
+      });
+  }, []);
+
+  const [id, idchange] = useState("");
+  const [firstname, firstnamechange] = useState("");
+  const [lastname, lastnamechange] = useState("");
+  const [email, emailchange] = useState("");
+  const [address, addresschange] = useState("");
+  const [, activechange] = useState(true);
+  const [validation, valchange] = useState(false);
+
+  const navigate = useNavigate();
+
+  const handlesubmit = (e) => {
+    e.preventDefault();
+    const prodata = { id, name, email, phone, active };
+    //put to edit localhost:5000/user/getUserDetails
+    fetch("http://localhost:8000/employee/" + userid, {
+      method: "PUT",
+      headers: { "content-type": "application/json" },
+      body: JSON.stringify(prodata),
+    })
+      .then((res) => {
+        alert("Saved successfully.");
+        navigate("/");
+      })
+      .catch((err) => {
+        console.log(err.message);
+      });
+  };
+  return (
+    <div>
+      <div className="row">
+        <div className="offset-lg-3 col-lg-6">
+          <form className="container" onSubmit={handlesubmit}>
+            <div className="card" style={{ textAlign: "left" }}>
+              <div className="card-title">
+                <h2>Employee Edit</h2>
+              </div>
+              <div className="card-body">
+                <div className="row">
+                  <div className="col-lg-12">
+                    <div className="form-group">
+                      <label>ID</label>
+                      <input
+                        value={id}
+                        disabled="disabled"
+                        className="form-control"
+                      ></input>
+                    </div>
+                  </div>
+
+                  <div className="col-lg-12">
+                    <div className="form-group">
+                      <label>First Name</label>
+                      <input
+                        required
+                        value={firstname}
+                        onMouseDown={(e) => valchange(true)}
+                        onChange={(e) => firstnamechange(e.target.value)}
+                        className="form-control"
+                      ></input>
+                      {name.length == 0 && validation && (
+                        <span className="text-danger">Enter the name</span>
+                      )}
+                    </div>
+                  </div>
+
+                  <div className="col-lg-12">
+                    <div className="form-group">
+                      <label>First Name</label>
+                      <input
+                        required
+                        value={lastname}
+                        onMouseDown={(e) => valchange(true)}
+                        onChange={(e) => lastnamechange(e.target.value)}
+                        className="form-control"
+                      ></input>
+                      {name.length == 0 && validation && (
+                        <span className="text-danger">Enter the name</span>
+                      )}
+                    </div>
+                  </div>
+
+                  <div className="col-lg-12">
+                    <div className="form-group">
+                      <label>Email</label>
+                      <input
+                        value={email}
+                        onChange={(e) => emailchange(e.target.value)}
+                        className="form-control"
+                      ></input>
+                    </div>
+                  </div>
+
+                  <div className="col-lg-12">
+                    <div className="form-group">
+                      <label>Address</label>
+                      <input
+                        value={address}
+                        onChange={(e) => address(e.target.value)}
+                        className="form-control"
+                      ></input>
+                    </div>
+                  </div>
+
+                  <div className="col-lg-12">
+                    <div className="form-check">
+                      <input
+                        checked={active}
+                        onChange={(e) => activechange(e.target.checked)}
+                        type="checkbox"
+                        className="form-check-input"
+                      ></input>
+                      <label className="form-check-label">Is Active</label>
+                    </div>
+                  </div>
+                  <div className="col-lg-12">
+                    <div className="form-group">
+                      <button className="btn btn-success" type="submit">
+                        Save
+                      </button>
+                      <Link to="/" className="btn btn-danger">
+                        Back
+                      </Link>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ProEdit;
