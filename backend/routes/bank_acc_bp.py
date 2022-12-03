0b36c5cb8f380b46fa1fb937107d0d754b697b02
aaron liew <aaron.liew.2019@scis.smu.edu.sg>
2022-12-03 16:30:44 +0800

    change getter data variable

--- backend/routes/bank_acc_bp.py
+++ backend/routes/bank_acc_bp.py
@@ -4,4 +4,4 @@ from controllers.BankAccountController import getAccInfo
 
 bank_acc_bp = Blueprint('bank_acc_bp', __name__)
 
-bank_acc_bp.route('/getAccountInfo', strict_slashes=False, methods=['GET'])(getAccInfo)
+bank_acc_bp.route('/getAccountInfo/<int:user_id>', strict_slashes=False, methods=['GET'])(getAccInfo)
