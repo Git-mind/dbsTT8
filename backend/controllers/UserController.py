78144ff900ae2c7fe15f81e3dfa735a17b8d908f
aaron liew <aaron.liew.2019@scis.smu.edu.sg>
2022-12-03 15:02:30 +0800

    update user details feature

--- backend/controllers/UserController.py
+++ backend/controllers/UserController.py
@@ -57,4 +57,50 @@ def getUserDetails():
     ), 400
 
 
+def updateUserDetails():
+    if request.is_json:
+        data = request.get_json()
+        user_id = data["UserID"]
+        newFirstname = data["Firstname"]
+        newLastname = data["Lastname"]
+        newEmail = data["Email"]
+        newAddress = data["Address"]
+        newOptIntoPhyStatements = data["OptIntoPhyStatements"]
+
+        try:
+            user = User.query.filter_by(UserID=user_id).first()
+            if user:
+                user.Firstname = newFirstname
+                user.Lastname = newLastname
+                user.Email= newEmail
+                user.Address = newAddress
+                user.OptIntoPhyStatements = newOptIntoPhyStatements
+                db.session.commit()
+                return jsonify(
+                    {
+                        "code": 200,
+                        "message": f'Successfully updated User {user_id}'
+                    }
+                ), 200
+            return jsonify(
+                {
+                    "code": 404,
+                    "data": "User not found."
+                }
+            )
+        except Exception as error:
+            return jsonify(
+                {
+                    "code": 500,
+                    "message": "Error updating user"
+                }
+            ), 500
+    else:
+        return jsonify(
+            {
+                "code": 400,
+                "data": str(data),
+                "message": "Invalid input type. pls input as json"
+            }
+        ), 400
 
--- backend/routes/user_bp.py
+++ backend/routes/user_bp.py
@@ -1,5 +1,6 @@
 from flask import Blueprint
-from controllers.UserController import getUserDetails
+from controllers.UserController import getUserDetails, updateUserDetails
 
 user_bp = Blueprint('user_bp', __name__)
-user_bp.route('/userDetails', strict_slashes=False, methods=['GET'])(getUserDetails)
+user_bp.route('/getUserDetails', strict_slashes=False, methods=['GET'])(getUserDetails)
+user_bp.route('/updateUserDetails', strict_slashes=False, methods=['POST'])(updateUserDetails)
