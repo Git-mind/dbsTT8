6deca9353f2bdde696fdab3c410044391679c8ce
tofu <tofuquant@gmail.com>
2022-12-03 12:07:14 +0800

    update naming

--- backend/routes/txn_bp.py
+++ backend/routes/txn_bp.py
@@ -1,5 +1,5 @@
 from flask import Blueprint
-from controllers.ViewTxnController import txn_detail
+from controllers.TxnController import txn_detail
 
 txn_bp = Blueprint('txn_bp', __name__)
 txn_bp.route('/txn_detail', strict_slashes=False, methods=['GET'])(txn_detail)
