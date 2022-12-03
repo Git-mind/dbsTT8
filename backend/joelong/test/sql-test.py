8eaa96c9027ec0931c1173afa8f0e2bd30edd9ce
tofu <tofuquant@gmail.com>
2022-12-03 11:05:13 +0800

    sql connection test

---
+++ backend/joelong/test/sql-test.py
@@ -0,0 +1,11 @@
+from sqlalchemy import create_engine
+user = "admin"
+password = "password"
+host = "database-1.cxepdeco3gac.ap-southeast-1.rds.amazonaws.com"
+port = 3306
+from sqlalchemy import create_engine
+engine = create_engine( f'mysql+pymysql://{user}:{password}@{host}:{port}/Bank',
+    connect_args={"ssl": {"ssl_ca": "DigiCertGlobalRootCA.crt.pem"}})
+with engine.connect() as con:
+    res = con.execute('SHOW TABLES')
+    print(res.fetchall())
