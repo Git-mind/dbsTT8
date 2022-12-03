ee174d24b0d8846cab8c1ff017c309c4ed0366c3
ricsg <96154243+ricsg@users.noreply.github.com>
2022-12-03 16:17:10 +0800

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,8 @@ import "./App.css";
 import { Route, Routes, useLocation } from "react-router-dom";
 import Login from "./pages/Login";
 import Dashboard from "./pages/Dashboard";
+import Profile from "./Profile";
+import ProEdit from "./ProfileEdit";
 
 function App() {
   return (
@@ -10,6 +12,8 @@ function App() {
       <Routes>
         <Route path="/" element={<Login />} />
         <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/profile" element={<Profile />}></Route>
+        <Route path="/profile/edit/:empid" element={<ProEdit />}></Route>
       </Routes>
     </div>
   );
