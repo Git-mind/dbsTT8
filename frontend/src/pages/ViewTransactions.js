0d17726579b2859bfee76138b2bef463fddbc67a
Clarice <claricegoh@gmail.com>
2022-12-03 16:21:35 +0800

    update axios call

--- frontend/src/pages/ViewTransactions.js
+++ frontend/src/pages/ViewTransactions.js
@@ -1,100 +1,84 @@
 import React, {useState} from "react";
-// import axios from "axios";
-// import hosturl from "../hosturl.js"
+import axios from "axios";
+import hosturl from "../hosturl.js"
 import { FaTimes } from 'react-icons/fa'
 
 // import { pullTransactions } from "../contexts/pullTransactions.js";
 const ViewTransactions = () => {
-    const [transactions, setTransactions] = useState([
-        {
-            "id": 1,
-            "AccountID": 621156213,
-            "ReceivingAccountID": 339657462,
-            "date": "2022-11-08T04:00:00.000Z",
-            "TransactionAmount": 500.00,
-            "comment": "Monthly Pocket Money"
-        },
-        {
-            "id": 2,
-            "AccountID": 958945214,
-            "ReceivingAccountID": 621156213,
-            "date": "2022-11-08T04:00:00.000Z",
-            "TransactionAmount": 8996.00,
-            "comment": "School Fees"
-        },
-        {
-            "id": 3,
-            "AccountID": 828120424,
-            "ReceivingAccountID": 322798030,
-            "date": "2022-11-25T04:00:00.000Z",
-            "TransactionAmount": 3000.00,
-            "comment": "Driving Centre Top-up"
-        },
-        {
-            "id": 4,
-            "AccountID": 353677039,
-            "ReceivingAccountID": 785703027,
-            "date": "2022-11-17T06:21:00.000Z",
-            "TransactionAmount": 255.00,
-            "comment": "Tuition Fee Payment"
-        },
-        {
-            "id": 5,
-            "AccountID": 259555772,
-            "ReceivingAccountID": 828120424,
-            "date": "2022-11-08T04:00:00.000Z",
-            "TransactionAmount": 32.00,
-            "comment": "Books Payment"
-        }
-    ])
-    // const accountId = 621156213;
-    // const transaction = pullTransactions();
+    // const [transactions, setTransactions] = useState([
+    //     {
+    //         "id": 1,
+    //         "AccountID": 621156213,
+    //         "ReceivingAccountID": 339657462,
+    //         "date": "2022-11-08T04:00:00.000Z",
+    //         "TransactionAmount": 500.00,
+    //         "comment": "Monthly Pocket Money"
+    //     },
+    //     {
+    //         "id": 2,
+    //         "AccountID": 958945214,
+    //         "ReceivingAccountID": 621156213,
+    //         "date": "2022-11-08T04:00:00.000Z",
+    //         "TransactionAmount": 8996.00,
+    //         "comment": "School Fees"
+    //     },
+    //     {
+    //         "id": 3,
+    //         "AccountID": 828120424,
+    //         "ReceivingAccountID": 322798030,
+    //         "date": "2022-11-25T04:00:00.000Z",
+    //         "TransactionAmount": 3000.00,
+    //         "comment": "Driving Centre Top-up"
+    //     },
+    //     {
+    //         "id": 4,
+    //         "AccountID": 353677039,
+    //         "ReceivingAccountID": 785703027,
+    //         "date": "2022-11-17T06:21:00.000Z",
+    //         "TransactionAmount": 255.00,
+    //         "comment": "Tuition Fee Payment"
+    //     },
+    //     {
+    //         "id": 5,
+    //         "AccountID": 259555772,
+    //         "ReceivingAccountID": 828120424,
+    //         "date": "2022-11-08T04:00:00.000Z",
+    //         "TransactionAmount": 32.00,
+    //         "comment": "Books Payment"
+    //     }
+    // ])
+    const accountId = "621156213";
+    // const transactions = pullTransactions();
     // get request
-    // async function getScheduledTransaction() {
-    //     try {
-    //         const response = await axios.get(hosturl + '/transaction/txn_detail');
-    //         console.log(response.data);
-    //     } catch (error) {
-    //         console.error(error.response.data);
-    //     }
-    // }
-    // post request
-    // async function createNewScheduledTransaction() {
-    //     try {
-    //         await axios
-    //             .post(hosturl + "/transaction/create_txn", {
-    //                 "AccountID": 621156213,
-    //                 "ReceivingAccountID": 339657462,
-    //                 "date": "2022-11-08T04:00:00.000Z",
-    //                 "TransactionAmount": 500.00,
-    //                 "comment": "Monthly Pocket Money"
-    //             })
-    //             .then((response) => {
-    //                 console.log(response.data);
-    //             });
-
-    //     } catch (error) {
-    //         console.log(error.response.data);
-    //     }
-    // }
-
-    // delete
-    // async function deleteScheduledTransaction() {
-    //     try {
-    //         await axios
-    //             .delete(hosturl + "/transaction/delete_txn/<txn_name>", {})
-    //             .then((response) => {
-    //                 console.log(response.data);
-    //             });
-
-    //     } catch (error) {
-    //         console.log(error.response.data);
-    //     }
-    // }
+    async function getScheduledTransactions() {
+        try {
+            var config ={headers:{
+                'Content-Type': 'application/json',
+                'Access-Control-Allow-Origin':'*'
+            },
+            body:{
+                'AccountID':accountId
+            }
+        }
+            let data = JSON.stringify({
+                AccountID: accountId
+            })
+            // console.log(data)pipe
+            const response = await axios.get(`http://localhost:5000/transaction/txn_detail?${data}`,config);
+            console.log(response.data);
+            console.log(response)
+            console.log("success!")
+            // const [transactions, setTransactions] = useState([response.data])
+        } catch (error) {
+            console.error(error.response.data);
+            console.log("failure")
+        }
+    }
+    const [transactions, setTransactions] = useState([getScheduledTransactions()])
 
     
     const deleteTransaction = (id) => {
-        setTransactions(transactions.filter((transaction) => transaction.id !== id))
+        setTransactions(transactions.filter((transaction) => transaction.TransactionID !== id))
     }
 
 
@@ -102,7 +86,7 @@ const ViewTransactions = () => {
         return (
             <>
                 {transactions.map((transaction) => (
-                    <Transaction key={transaction.id} transaction={transaction} onDelete={onDelete} onToggle={onToggle} />
+                    <Transaction key={transaction.TransactionID} transaction={transaction} onDelete={onDelete} onToggle={onToggle} />
                 ))}
             </>
         )
@@ -110,11 +94,11 @@ const ViewTransactions = () => {
 
 
     const Transaction = ({ transaction, onDelete, onToggle }) => {
-        const displayDate = transaction.date.split('T')[0]
+        const displayDate = transaction.Date.split('T')[0]
         return (
             <div className="transaction">
-                <h3>{transaction.AccountID} <FaTimes style={{ color: 'red', cursor: 'pointer' }} onClick={() => onDelete(transaction.id)} /></h3>
-                <h5>{transaction.comment}</h5>
+                <h3>{transaction.AccountID} <FaTimes style={{ color: 'red', cursor: 'pointer' }} onClick={() => onDelete(transaction.TransactionID)} /></h3>
+                <h5>{transaction.Comment}</h5>
                 <p>{displayDate}</p>
                 <h4 align="right">${transaction.TransactionAmount}</h4>
             </div>
