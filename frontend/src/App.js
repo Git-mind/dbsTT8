fc78aa9dad5e1f346a78c61eca8d39c38b4783c0
ricsg <96154243+ricsg@users.noreply.github.com>
2022-12-03 15:57:55 +0800

    unfork

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,18 +1,98 @@
-import logo from "./logo.svg";
-import "./App.css";
-import { Route, Routes, useLocation } from "react-router-dom";
-import Login from "./pages/Login";
-import Dashboard from "./pages/Dashboard";
+import { useEffect, useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
 
-function App() {
+const Profile = () => {
+  const [profdata, profdatachange] = useState(null);
+  const navigate = useNavigate();
+
+  const LoadDetail = (id) => {
+    navigate("/employee/detail/" + id);
+  };
+  const LoadEdit = (id) => {
+    navigate("/employee/edit/" + id);
+  };
+  const Removefunction = (id) => {
+    if (window.confirm("Do you want to remove?")) {
+      fetch("http://localhost:8000/employee/" + id, {
+        method: "DELETE",
+      })
+        .then((res) => {
+          alert("Removed successfully.");
+          window.location.reload();
+        })
+        .catch((err) => {
+          console.log(err.message);
+        });
+    }
+  };
+
+  useEffect(() => {
+    fetch("https://mocki.io/v1/975c7fac-bb98-41ae-b9a0-374d3b72871f")
+      .then((res) => {
+        return res.json();
+      })
+      .then((resp) => {
+        profdatachange(resp);
+      })
+      .catch((err) => {
+        console.log(err.message);
+      });
+  }, []);
   return (
-    <div>
-      <Routes>
-        <Route path="/" element={<Login />} />
-        <Route path="/dashboard" element={<Dashboard />} />
-      </Routes>
+    <div className="container">
+      <div className="card">
+        <div className="card-title">
+          <h2>Profile Page</h2>
+        </div>
+        <div className="card-body">
+          <div className="divbtn"></div>
+          <table className="table table-bordered">
+            <thead className="bg-dark text-white">
+              <tr>
+                <td>Username</td>
+                <td>Firstname</td>
+                <td>Lastname</td>
+                <td>Email:</td>
+                <td>Address</td>
+                <td>OptIntoPhyStatements</td>
+              </tr>
+            </thead>
+            <tbody>
+              {profdata &&
+                profdata.map((item) => (
+                  <tr key={item.id}>
+                    <td>{item.Username}</td>
+                    <td>{item.Firstname}</td>
+                    <td>{item.Lastname}</td>
+                    <td>{item.Email}</td>
+                    <td>{item.Address}</td>
+                    <td>{item.OptIntoPhyStatements}</td>
+                    <td>
+                      <a
+                        onClick={() => {
+                          Removefunction(item.id);
+                        }}
+                        className="btn btn-danger"
+                      >
+                        Remove
+                      </a>
+                      <a
+                        onClick={() => {
+                          LoadEdit(item.id);
+                        }}
+                        className="btn btn-primary"
+                      >
+                        Edit
+                      </a>
+                    </td>
+                  </tr>
+                ))}
+            </tbody>
+          </table>
+        </div>
+      </div>
     </div>
   );
-}
+};
 
-export default App;
+export default Profile;
