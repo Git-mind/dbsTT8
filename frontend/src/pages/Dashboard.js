4d79b59951964305c2a2294563002059a172eee1
Jasmine Chen <jasminechenyuqinggg@gmail.com>
2022-12-03 12:04:34 +0800

    initial commit for dashboard

---
+++ frontend/src/pages/Dashboard.js
@@ -0,0 +1,17 @@
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+// import AuthService from "../services/auth.service";
+// import DbsLogo from "../assets/DBS-Bank-logo.png";
+// import { useAuth } from "../contexts/authContext";\
+
+const Dashboard = () => {
+  return (
+    <>
+      <div>
+        <p> hello world</p>
+      </div>
+    </>
+  );
+};
+
+export default Dashboard;
