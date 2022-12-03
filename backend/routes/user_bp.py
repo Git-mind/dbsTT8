3855d61609bca4d8292a317bee635a4924129271
aaron liew <aaron.liew.2019@scis.smu.edu.sg>
2022-12-03 16:34:06 +0800

    changed route variable type for getUserDetails

--- backend/routes/user_bp.py
+++ backend/routes/user_bp.py
@@ -2,5 +2,5 @@ from flask import Blueprint
 from controllers.UserController import getUserDetails, updateUserDetails
 
 user_bp = Blueprint('user_bp', __name__)
-user_bp.route('/getUserDetails', strict_slashes=False, methods=['GET'])(getUserDetails)
+user_bp.route('/getUserDetails/<int:user_id>', strict_slashes=False, methods=['GET'])(getUserDetails)
 user_bp.route('/updateUserDetails', strict_slashes=False, methods=['POST'])(updateUserDetails)
