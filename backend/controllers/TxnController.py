574227c7c47eaf82e8fe6d5a2f087349264cec78
Xianweiii <68322946+Git-mind@users.noreply.github.com>
2022-12-03 12:58:41 +0800

    Added error handling to txn details functionality

--- backend/controllers/TxnController.py
+++ backend/controllers/TxnController.py
@@ -8,31 +8,21 @@ from models.db_models import ScheduledTransaction
 # http://127.0.0.1:5000/transaction/create_transaction?username=user101
 
 def txn_detail():
-    # try:
-    #     db.session.add(transaction)
-    #     db.session.commit()
-    # except Exception as e:
-    #     print(e)
-    #     return jsonify(
-    #         {
-    #             "code": 500,
-    #             "data": {
-    #                 "transaction": transaction
-    #             },
-    #             "message": "An error occurred creating the transaction."
-    #         }
-    #     ), 500
 
     TransactionID = request.json.get("TransactionID", None)
     AccountID = request.json.get("AccountID", None)
     txn_detail = ScheduledTransaction.query.filter_by(TransactionID=TransactionID, AccountID=AccountID).first()
-    print(TransactionID)
-    print(AccountID)
-    print(txn_detail.json())
 
+    if txn_detail:
+        return jsonify(
+            {
+                "code": 200,
+                "data": txn_detail.json()
+            }
+        )
     return jsonify(
         {
-            "code": 201,
-            "data": txn_detail.json()
+            "code": 404,
+            "message": "Transaction not found."
         }
-    ), 201
+    ), 404
